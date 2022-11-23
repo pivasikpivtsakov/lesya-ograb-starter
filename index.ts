4255e85a5434b9105459c95e125db0b4c676b368
pivasikpivtsakov <elda250@live.com>
2022-11-24 00:31:38 +0600

    run on heroku

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import routes from "./routes";
 const init = async () => {
     const server = Hapi.server({
         port: process.env.PORT || 3000,
-        host: 'localhost'
+        host: '0.0.0.0'
     });
 
     // rest
