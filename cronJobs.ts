68c11c72836478261280751314e8e979d49ed17c
pivasikpivtsakov <elda250@live.com>
2022-11-27 18:55:24 +0600

    try removing async

--- cronJobs.ts
+++ cronJobs.ts
@@ -1,14 +1,20 @@
 import {schedule} from "node-cron";
 import soloFight from "./tasks/soloFight";
 import ograbStart from "./tasks/ograbStart";
+import openCases from "./tasks/openCases";
 
 
 const every10Mins = '*/10 * * * *';
-schedule(every10Mins, async now => {
-    await soloFight();
+schedule(every10Mins, now => {
+    soloFight().then(console.log);
 });
 
 const onceADayAt18 = '0 18 * * *';
-schedule(onceADayAt18, async now => {
-    await ograbStart();
+schedule(onceADayAt18, now => {
+    ograbStart().then(console.log);
+});
+
+const every30Mins = '*/10 * * * *';
+schedule(every30Mins, now => {
+    openCases().then(console.log);
 });
