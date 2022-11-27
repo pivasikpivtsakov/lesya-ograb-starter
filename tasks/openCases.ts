48f7f0b7cb8d4aa6cc4439dc71c2994884d22e44
pivasikpivtsakov <elda250@live.com>
2022-11-27 21:35:54 +0600

    opencases 30 cases in a run

--- tasks/openCases.ts
+++ tasks/openCases.ts
@@ -4,8 +4,10 @@ import {delay} from "../requests/common";
 
 export default async () => {
     console.log('opencases started')
-    for (let i = 1; i <= 7; i++) {
-        await messagesSendMany({message: `открыть кейс ${i}`, tokens: await getAllTokens()});
-        await delay(500);
+    for (let times = 0; times < 30; times++) {
+        for (let i = 1; i <= 7; i++) {
+            await messagesSendMany({message: `открыть кейс ${i}`, tokens: await getAllTokens()});
+            await delay(500);
+        }
     }
 };
