df184c054c9095239901be72efc426444987c36e
pivasikpivtsakov <elda250@live.com>
2022-11-27 20:14:43 +0600

    delay in opencases

--- tasks/openCases.ts
+++ tasks/openCases.ts
@@ -1,9 +1,11 @@
 import {messagesSendMany} from "../requests/messagesSend";
 import {getAllTokens} from "../models/tokens";
+import {delay} from "../requests/common";
 
 export default async () => {
     console.log('opencases started')
     for (let i = 1; i <= 7; i++) {
         await messagesSendMany({message: `открыть кейс ${i}`, tokens: await getAllTokens()});
+        await delay(500);
     }
 };
